87996ade45472c89738a69f99e26a5237347b038
renadgharz <renad.gharz@outlook.com>
2023-08-06 14:29:25 -0400

    created get_token() method to validate and refresh tokens

--- src/connection.py
+++ src/connection.py
@@ -2,7 +2,9 @@ from dotenv import load_dotenv
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from flask import url_for
+from flask import url_for, redirect, session
+import time
+from config import config
 
 dotenv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 
                                            '..', '.env'))
@@ -18,3 +20,19 @@ def create_spotify_oauth():
         redirect_uri=url_for("authorize", _external=True),
         scope="playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-library-read"
     )
+
+def get_token():
+    global token_info
+    token_info = session.get(config["TOKEN_INFO"], None)
+    
+    if not token_info:
+        raise "exception"
+    
+    now = int(time.time())
+    is_expired = token_info['expires_at'] - now < 60
+ 
+    if (is_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+ 
+    return token_info
