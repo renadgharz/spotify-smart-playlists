45f3ebe17801cb43246d743d0a8b907723f6b4a4
renadgharz <renad.gharz@outlook.com>
2023-09-18 12:01:44 -0400

    formatting touch ups

--- src/connection.py
+++ src/connection.py
@@ -1,38 +1,46 @@
+# api_connection.py
 from dotenv import load_dotenv
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from flask import url_for, redirect, session
 import time
-from config import config
+from flask import url_for, redirect
 
-dotenv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 
-                                           '..', '.env'))
+dotenv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '.env'))
 load_dotenv(dotenv_path)
 
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
 
 def create_spotify_oauth():
     return SpotifyOAuth(
         client_id=client_id,
         client_secret=client_secret,
-        redirect_uri=url_for("authorize", _external=True),
-        scope="playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-library-read"
+        redirect_uri=url_for(redirect('authorize')),  # Update with your redirect URI
+        scope='playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-library-read'
     )
 
 def get_token():
-    global token_info
-    token_info = session.get(config["TOKEN_INFO"], None)
-    
-    if not token_info:
-        raise "exception"
+    sp_oauth = create_spotify_oauth()
+    token_info = sp_oauth.get_access_token()
     
     now = int(time.time())
     is_expired = token_info['expires_at'] - now < 60
  
-    if (is_expired):
-        sp_oauth = create_spotify_oauth()
+    if is_expired:
         token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
  
     return token_info
+
+def get_tracks(playlists, token_info):
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    
+    all_tracks = []
+    
+    for playlist in playlists:
+        tracks = sp.playlist_items(playlist_id=playlist)
+        all_tracks.extend(tracks['items'])
+        
+    return all_tracks
+    
+    
